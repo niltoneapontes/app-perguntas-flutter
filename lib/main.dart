7d66900eabe1c4b7e20016e9c5477547f5dde892
Nilton Pontes <niltoneapontes@gmail.com>
2021-09-11 16:11:06 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// main() {
+//   runApp(new PerguntaApp());
+// }
+
+main() => runApp(
+    new PerguntaApp()); //recomenda-se usar arrow function aqui (em flutter só pode ter uma linha na arrow)
+
+class PerguntaApp extends StatelessWidget {
+  // estou dizendo que o método build deve ser obrigatoriamente como passado
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Faça sua pergunta'),
+      ),
+    );
+  }
+}
